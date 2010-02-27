575499445152c7f4053c3117819b940fbcc8af8f
hamiltont <hamiltont@0ea79a6c-093b-11df-ae69-07492475f941>
2010-02-27 22:28:40 +0000

    Added information about retrieving number of available spots. Added a file that would allow you to request info on a single lot, as well as updated the XML that lots should return to include a few more items specific to available spots

---
+++ trunk/Web Server/available_spots.php
@@ -0,0 +1,15 @@
+<?php
+// ============================================================================
+//
+//  File: available_spots.php
+//  Parameters: lot
+//  Purpose: Return the number of spaces in a single lot. Use the lot ID string 
+// 		     as the parameter value
+//  ToDo: Create me 
+//
+// ===========================================================================
+
+
+
+
+?>
--- trunk/Web Server/lots.php
+++ trunk/Web Server/lots.php
@@ -7,7 +7,8 @@
 //  Purpose: List all available lots. Currently does this via a simple CSV
 //           set. These names are the lot ids that are passed into latest.php. 
 //           At a later point this should return XML, with a Lot Name, Lot ID, 
-//           and any other params
+//           Available Spaces, Time each available space was calculated, 
+// 		     and any other params
 //  ToDo: Eventually make this return XML
 //
 // ===========================================================================
