f7bace1dc6c30ca3015a4607067448707ff79688
hamiltont <hamiltont@0ea79a6c-093b-11df-ae69-07492475f941>
2010-02-27 20:06:20 +0000

    Added a header to each file with the TODO info, parameters, etc. Added a lots file which will return info about the lots available

--- Web Server/latest.php
+++ Web Server/latest.php
@@ -1,4 +1,18 @@
 <?php
+// ============================================================================
+//
+//  File: latest.php
+//  Parameters: lot, html=1
+//  Purpose: Shows the most recent image. 
+//            - If passed html=0, then returns the image data itself, rather 
+//              than printing out an <img /> tag. 
+//            - If passed lot id, then the most recent image whose title contains 
+//              the lot id is selected and returned
+//  ToDo: Setup the lot and pretty params
+//
+// ===========================================================================
+
+
 set_time_limit(0);
 
 header("Content-type: image/jpeg");
