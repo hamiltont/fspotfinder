c6d33e3ae7030062c118c0e2cb45d0aa89bc3b78
hamiltont <hamiltont@0ea79a6c-093b-11df-ae69-07492475f941>
2010-02-27 22:36:06 +0000

    Slightly modified the description to point our that the file not only lists available lots, but also describes the state of the lots.

--- Web Server/lots.php
+++ Web Server/lots.php
@@ -4,9 +4,11 @@
 //
 //  File: lots.php
 //  Parameters: none
-//  Purpose: List all available lots. Currently does this via a simple CSV
-//           set. These names are the lot ids that are passed into latest.php. 
-//           At a later point this should return XML, with a Lot Name, Lot ID, 
+//  Purpose: List all available lots, and describe the state of each lot. 
+// 			 Currently does this via a simple CSV set. These names are the 
+// 		     lot ids that are passed into latest.php. 
+//           
+//			 At a later point this should return XML, with a Lot Name, Lot ID, 
 //           Available Spaces, Time each available space was calculated, 
 // 		     the URI to the latest photo, the timestamp on the photo, and 
 //		     any other params
