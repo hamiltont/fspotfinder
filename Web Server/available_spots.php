26e60a137abc70a7d4bfb8475a3bc19e43d06c1d
hamiltont <hamiltont@0ea79a6c-093b-11df-ae69-07492475f941>
2010-02-27 22:47:17 +0000

    Modified the purpose of available_spots. Now it should also provide enough information that a phone will be able to tell if if needs to re-request the latest.php file

--- Web Server/available_spots.php
+++ Web Server/available_spots.php
@@ -3,9 +3,14 @@
 //
 //  File: available_spots.php
 //  Parameters: lot
-//  Purpose: Return the number of spaces in a single lot. Use the lot ID string 
-// 		     as the parameter value
-//  ToDo: Create me 
+//  Purpose: Return the number of spaces in a single lot, as well as just enough
+//   	     info for the phone to know if it needs to request a new picture from
+//			 latest.php. We want this to be small, so an optimal response would
+// 			 be something like "4,1267314230" aka "spots,timestamp_of_some_sort"
+//
+//			Use the lot ID string as the parameter value.
+//  ToDo: Create me (also, perhaps rename the file to something that matches it's
+// 		  responsibilities more
 //
 // ===========================================================================
 
