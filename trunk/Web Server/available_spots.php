398ca53da7b76c955141f0f5e34a8ea5c1f1994d
adammalbright <adammalbright@0ea79a6c-093b-11df-ae69-07492475f941>
2010-03-02 21:22:58 +0000

    - Updated the web server scripts to read from the database and export XML files

--- trunk/Web Server/available_spots.php
+++ trunk/Web Server/available_spots.php
@@ -4,9 +4,9 @@
 //  File: available_spots.php
 //  Parameters: lot
 //  Purpose: Return the number of spaces in a single lot, as well as just enough
-//   	     info for the phone to know if it needs to request a new picture from
-//			 latest.php. We want this to be small, so an optimal response would
-// 			 be something like "4,1267314230" aka "spots,timestamp_of_some_sort"
+//     	     info for the phone to know if it needs to request a new picture from
+//	     		 latest.php. We want this to be small, so an optimal response would
+// 			     be something like "4,1267314230" aka "spots,timestamp_of_some_sort"
 //
 //			Use the lot ID string as the parameter value.
 //  ToDo: Create me (also, perhaps rename the file to something that matches it's
@@ -15,6 +15,7 @@
 // ===========================================================================
 
 
+// do we really need this.  lots.php has all of this info?
 
 
 ?>
--- trunk/Web Server/latest.php
+++ trunk/Web Server/latest.php
@@ -4,28 +4,38 @@
 //  File: latest.php
 //  Parameters: lot, html=1
 //  Purpose: Shows the most recent image. 
-//            - If passed html=0, then returns the image data itself, rather 
-//              than printing out an <img /> tag. 
+//            - If passed html=1, print out an <img /> tag rather than
+//              return the image data itself
 //            - If passed lot id, then the most recent image whose title contains 
 //              the lot id is selected and returned
-//  ToDo: Setup the lot and pretty params
 //
 // ===========================================================================
 
 
 set_time_limit(0);
 
-header("Content-type: image/jpeg");
-									
+$lot = isset($_GET['lot'])? strip_tags($_GET['lot']): false;
+
 $path = dirname(realpath(__FILE__));
 $images = scandir($path,1);
-$count = 20;
-foreach($images as $im){
-	if(($count--) > 0 && strpos($im,'jpg') != 0){
-    $im_path = $path . '/' .$im;
-    readfile($im_path);
-    exit(0);
-	}
-}
 
+foreach($images as $im){
+
+  // Scan all the images files (most recent file) looking for
+  // an image from a particular lot (if specified)
+	if(strpos($im,'jpg') != 0 && (!$lot || stripos($im,$lot) !== false) ){
+
+    //HTML Mode?
+    if(isset($_GET['html']) && $_GET['html'] == 1){
+      echo '<img src="' . $im . '" />';
+    }
+    else {
+      header("Content-type: image/jpeg");
+      $im_path = $path . '/' .$im;
+      readfile($im_path);
+    }
+
+    exit(0);
+  }
+}
 ?>
